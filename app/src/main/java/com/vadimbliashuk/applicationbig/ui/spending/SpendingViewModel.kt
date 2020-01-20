1692bc2e09ef2a4defac81d5384ba549a87b37c3
Khadgar <blyashuk@list.ru>
2020-01-20 12:32:14 +0300

    Update viewModel for SpendingFragment

--- app/src/main/java/com/vadimbliashuk/applicationbig/ui/spending/SpendingViewModel.kt
+++ app/src/main/java/com/vadimbliashuk/applicationbig/ui/spending/SpendingViewModel.kt
@@ -1,7 +1,29 @@
 package com.vadimbliashuk.applicationbig.ui.spending
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.vadimbliashuk.applicationbig.db.SpendingDatabase
+import com.vadimbliashuk.applicationbig.db.SpendingRepository
+import com.vadimbliashuk.applicationbig.model.SpendingEntity
+import kotlinx.coroutines.launch
 
-class SpendingViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SpendingViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: SpendingRepository
+    val allSpending: LiveData<List<SpendingEntity>>
+
+    init {
+        val spendingDao = SpendingDatabase.getInstance(application).spendingDao()
+        repository = SpendingRepository(spendingDao)
+        allSpending = repository.allSpending
+    }
+
+    fun insert(spending: SpendingEntity) = viewModelScope.launch {
+        repository.insert(spending)
+    }
+
+    fun delete(spending: SpendingEntity) = viewModelScope.launch {
+        repository.delete(spending)
+    }
 }
